36c550c05e79dcfa048327f153bebf41706cc01d
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 16:17:08 +0530

    Add generate a birth

--- dice.sh
+++ dice.sh
@@ -1,23 +1,4 @@
-#Roll a die and find the number between 1 to 6
-die1=$(( RANDOM%6+1 ))
-echo "Die 1: $die1"
-
-#Repeat the Die roll and find the result each time
-die2=$(( RANDOM%6+1 ))
-echo "Die 2: $die2"
-
-#Store the result in a dictionary
-declare -A dice
-dice[die1]=$die2
-
-#Repeat till any one of the number has reached 10 times in shell script
-for (( i=0; i<=10; i++ ))
-do
-   for(( j=1; j<=6; j++ ))
-   do
-      if [[ ${dice[$j]} -eq 10 ]]
-      then
-         echo "$j has reached 10 times"
-      fi
-   done
-done
+for ((i=1;i<=50;i++)) 
+do 
+echo $((RANDOM%12+1)) 
+done | sort -n | uniq -c
